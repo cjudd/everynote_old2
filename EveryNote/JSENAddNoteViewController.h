91e78b84f049522034e9d9445ec7aa07ccdf65f7
Christopher M. Judd <javajudd@gmail.com>
2012-06-28 06:37:18 -0400

    Added Notes List and Notes Add Screens.

---
+++ EveryNote/JSENAddNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSENAddNoteViewController.h
+//  EveryNote
+//
+//  Created by Christopher Judd on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSENAddNoteViewController : UITableViewController
+
+@end
